ea3727d7d056633c22d89e135d5f65cc3796a788
Kyle Ramos <kyleramos17@gmail.com>
2021-11-10 18:19:43 -0500

    reformatting of subtitles

--- readme.py
+++ readme.py
@@ -7,73 +7,34 @@ from rich.tree import Tree
 console = Console(record=True, width=100)
 
 tree = Tree(
-    "🙂 [link=https://koaning.io]Vincent D. Warmerdam", guide_style="bold bright_black"
+    ":blush: [link=https://github.com/Kyle-f-r]kyle ramos",
+    guide_style="bold bright_black",
 )
-python_tree = tree.add("📦 Open Source Packages", guide_style="bright_black")
-python_tree.add(
-    "[bold link=https://scikit-lego.netlify.app/]scikit-lego[/] - [bright_black]lego bricks for sklearn"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]human-learn[/] - [bright_black]rule-based components for sklearn"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]whatlies[/]    - [bright_black]word embeddings for sklearn pipelines"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]clumper[/]     - [bright_black]function API for lists of dicts"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]evol[/]        - [bright_black]grammar for genetic heuristics"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]memo[/]        - [bright_black]saves a whole log of time"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]drawdata[/]    - [bright_black]draw datasets in jupyter"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]tokenwiser[/]  - [bright_black]sklearn token tricks via .fit_partial()"
-)
-python_tree.add(
-    "[bold link=https://koaning.github.io/human-learn/]mktestdocs[/]  - [bright_black]turn markdown files into pytest tests"
-)
-online_tree = tree.add("⭐ Online Projects", guide_style="bright_black")
+online_tree = tree.add(":wave: hey, i'm kyle", guide_style="bright_black")
 online_tree.add(
-    "[bold link=https://koaning.io]koaning.io[/]   - [bright_black]personal blog"
+    "[bold link=https://github.com/Kyle-f-r]ramos.io[/]              - [bright_black]resume and professional statement"
 )
 online_tree.add(
-    "[bold link=https://calmcode.io]calmcode.io[/]  - [bright_black]dev education service"
+    "[bold link=https://https://www.linkedin.com/in/kyle-ramos-339625126?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZa2HExBwRy%2BVw73mcaiGcw%3D%3D]linkedin/kyle-ramos[/]   - [bright_black]personal linkedin page"
 )
-online_tree.add(
-    "[bold link=https://calmcode.io]dearme.email[/] - [bright_black]reflection service"
+
+python_tree = tree.add(":seedling: Projects and Interests", guide_style="bright_black")
+python_tree.add(
+    "[bold link=https://github.com/Kyle-f-r]condition-monitoring[/]  - [bright_black]anomoly detection for MOCVD reactors"
 )
-talk_tree = tree.add("🎙️ Popular Talks", guide_style="bright_black")
-talk_tree.add(
-    "[bold link=https://youtu.be/qcrR-Hd0LhI?t=542]Optimal Benchmarks and Other Failures[/]"
+python_tree.add(
+    "[bold link=https://github.com/Kyle-f-r]emissions[/]             - [bright_black]studying time-series forecasting methods"
 )
-talk_tree.add(
-    "[bold link=https://www.youtube.com/watch?v=nJAmN6gWdK8]Playing by the Rules-Based-Systems[/]"
+python_tree.add(
+    "[bold link=https://github.com/Kyle-f-r]prodeus.app[/]           - [bright_black]user machine learning"
 )
-talk_tree.add(
-    "[bold link=https://www.youtube.com/watch?v=Z8MEFI7ZJlA]How to Constrain Artificial Stupidity[/]"
-)
-talk_tree.add(
-    "[bold link=https://www.youtube.com/watch?v=kYMfE9u-lMo]The Profession of Solving the Wrong Problem[/]"
-)
-talk_tree.add(
-    "[bold link=https://www.youtube.com/watch?v=68ABAU_V8qI]Winning with Simple, even Linear, Models[/]"
-)
-employer_tree = tree.add("👨‍💻 Employer", guide_style="bright_black")
-employer_tree.add(
-    "[bold link=https://rasa.com]Rasa[/] - [bright_black]conversational software"
+python_tree.add(
+    "[bold link=https://github.com/Kyle-f-r]paintings.me[/]          - [bright_black]personal painting studies"
 )
 
 
 console.print(tree)
 console.print("")
-console.print(
-    "[green]Follow me on twitter [bold link=https://twitter.com/fishnets88]@fishnets88[/]"
-)
 
 CONSOLE_HTML_FORMAT = """\
 <pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>

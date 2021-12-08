f621c3e51e6013fc899bccf99c6d1856a9de1488
Kyle Ramos <kyleramos17@gmail.com>
2021-12-08 15:18:26 -0500

    update

--- readme.py
+++ readme.py
@@ -9,7 +9,7 @@ tree = Tree(
 )
 online_tree = tree.add(":wave: hey, i'm kyle", guide_style="bright_black")
 online_tree.add(
-    "[bold link=https://github.com/Kyle-f-r]ramos.io[/]              - [bright_black]resume and professional statement"
+    "[bold link=https://github.com/Kyle-f-r]ramos.io[/]              - [bright_black]personal website"
 )
 online_tree.add(
     "[bold link=https://www.linkedin.com/in/kyle-ramos-339625126/]linkedin/kyle-ramos[/]   - [bright_black]personal linkedin page"
@@ -18,24 +18,18 @@ online_tree.add(
 
 python_tree = tree.add(":seedling: Projects and Interests", guide_style="bright_black")
 python_tree.add(
-    "[bold link=https://kyle-f-r.github.io/condition-monitoring/]condition-monitoring[/]  - [bright_black]anomoly detection for MOCVD reactors"
+    "[bold link=https://github.com/Kyle-f-r/Conditional-Monitoring[/]  - [bright_black]anomoly detection for MOCVD reactors"
 )
 python_tree.add(
     "[bold link=https://github.com/Kyle-f-r]prodeus.app[/]           - [bright_black]user machine learning"
 )
-python_tree.add(
-    "[bold link=https://github.com/Kyle-f-r]paintings.me[/]          - [bright_black]personal painting studies"
-)
 
 python_tree = tree.add(":microscope: Coursework", guide_style="bright_black")
 python_tree.add(
-    "[bold link=https://github.com/Kyle-f-r]time-series emissions[/] - [bright_black]studying time-series forecasting methods"
+    "[bold link=https://github.com/Kyle-f-r/Time-Series-Carbon-Emission-Forecasting[/] - [bright_black]studying time-series forecasting methods"
 )
 python_tree.add(
-    "[bold link=https://github.com/Kyle-f-r]deep learning[/]         - [bright_black]Recognizing the Street View Housing Number Digit using Neural Networks"
-)
-python_tree.add(
-    "[bold link=https://github.com/Kyle-f-r]recommendation sys[/]    - [bright_black]Building a recommendation system to recommend the best Amazon products"
+    "[bold link=https://github.com/Kyle-f-r/Loan-Eligibility-Prediction[/]         - [bright_black]loan eligibility classifier"
 )
 
 console.print(tree)
